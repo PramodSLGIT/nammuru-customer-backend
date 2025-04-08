6ceeb3964cd3f0d400f64218ff9b053475741e2f
Pramod Sannagowdru <pramodsannagowdru@gmail.com>
2025-04-08 20:49:57 +0530

    the registration and login changed

--- api/api.go
+++ api/api.go
@@ -1,19 +1,22 @@
 package api
 
 import (
+	"log"
 	"nammuru-backend/controllers"
+	"nammuru-backend/jwtauth"
 	"net/http"
 
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func NammuruApi(){
-	// gin.SetMode(gin.ReleaseMode)
-	gin.SetMode(gin.DebugMode)
+func NammuruApi() {
+	gin.SetMode(gin.ReleaseMode)
+	// gin.SetMode(gin.DebugMode)
 
 	router := gin.Default()
-	config:= cors.DefaultConfig()
+	config := cors.DefaultConfig()
 
 	config.AllowAllOrigins = true
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
@@ -22,13 +25,30 @@ func NammuruApi(){
 
 	router.Use(cors.New(config))
 
+	authMiddleware := jwtauth.InitJwt()
+	router.POST("c1/login", authMiddleware.LoginHandler)
+
+	router.NoRoute(authMiddleware.MiddlewareFunc(), func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims: %#v\n", claims)
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+	p1 := router.Group("/c1")
+	{
+		p1.POST("/logout", authMiddleware.LogoutHandler)
+		var user = new(controllers.UserRegistrationController)
+		p1.POST("/register", user.Register)
+	}
 	c1 := router.Group("/c1")
 	{
 		user := new(controllers.UserRegistrationController)
-		c1.POST("/registration", user.Register)
 		c1.GET("/otp/:phonenumber", user.OtpGeneration)
 		c1.GET("/verify/:phonenumber/:otp", user.Login)
 		c1.POST("/profileimage", user.AddProfileImage)
+
+		// customer := new(controllers.CustomerController)
+		// router.GET("/ws", customer.CustomerWebSocket)
 	}
 
 	router.NoRoute(func(c *gin.Context) {
--- forms/userform.go
+++ forms/userform.go
@@ -11,16 +11,31 @@ const (
 	RideCollection     = "ride_info"
 )
 
+type UserRegistrationForm struct {
+	ProfileImage string `json:"profile_image"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	PhoneNumber  string `json:"phone_number"`
+	Gender       string `json:"gender"`
+	Password     string `json:"password"`
+}
+
 type UserModel struct {
 	ProfileImage []byte `json:"profile_image" bson:"profile_image" binding:"-"`
 	Name         string `json:"name" bson:"name"`
 	Email        string `json:"email" bson:"email"`
 	PhoneNumber  string `json:"phone_number" bson:"phone_number"`
 	Gender       string `json:"gender" bson:"gender"`
+	Password     string `json:"password"`
 }
 
 type LoginUserModel struct {
 	PhoneNumber string     `json:"phone_number" bson:"phone_number"`
 	Otp         string     `json:"otp" bson:"otp"`
 	Time        *time.Time `json:"time" bson:"time"`
+}
+
+type LoginModel struct {
+	PhoneNumber string `json:"phone_number" binding:"required"`
+	Password    string `json:"password" binding:"required"`
 }
